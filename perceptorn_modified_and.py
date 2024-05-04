b21ca3c143c6e7aa1a1b3182e1a351a0600a124e
Aayush Chaudhary <acaayush1111@gmail.com>
2024-05-04 23:56:28 +0530

    perceptron modified AND

---
+++ perceptorn_modified_and.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+#and gate       x1, x2, special bias 1
+t1 = np.array([0,0])
+t2 = np.array([0,1])
+t3 = np.array([1,0])
+t4 = np.array([1,1])
+#weights 1,1, special bias -1
+w = np.array([1,1])
+
+# shift bias to here instead of input 
+b = -1
+
+def perceptron(x, w, b = 0.5): 
+   if(( np.dot(w,x) + b) > 0):
+      return 1
+   else: return 0
+
+
+y = perceptron(t1, w, b)
+print(y)
+y = perceptron(t2, w, b)
+print(y)
+y = perceptron(t3, w, b)
+print(y)
+y = perceptron(t4, w, b)
+print(y)
+
