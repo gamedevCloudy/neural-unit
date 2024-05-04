1931e8a15842f8f19e16cb318614e5c4f919be61
Aayush Chaudhary <acaayush1111@gmail.com>
2024-05-04 23:58:16 +0530

    perceptron or modified

--- perceptron_modified_OR.py
+++ perceptron_modified_OR.py
@@ -1,17 +1,17 @@
 import numpy as np
 
 
-#and gate  x1, x2
+#and gate     x1, x2,
 t1 = np.array([0,0])
 t2 = np.array([0,1])
 t3 = np.array([1,0])
 t4 = np.array([1,1])
-#weights 1,1
+#weights 1,1, 
 w = np.array([1,1])
 
 # shift bias to here instead of input 
 # for or bias is 0, and is 1
-b = -1
+b = 0
 
 def perceptron(x, w, b = 0.5): 
    if(( np.dot(w,x) + b) > 0):
